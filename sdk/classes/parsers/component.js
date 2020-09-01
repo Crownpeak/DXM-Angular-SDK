a51573f8e3aacae5144315318b3e823facef8afc
Richard Lund <richard.lund@crownpeak.com>
2020-09-01 10:09:24 +0100

    Issue #7, add support for placeholders and replacements when scaffolding.

--- sdk/classes/parsers/component.js
+++ sdk/classes/parsers/component.js
@@ -134,6 +134,10 @@ const trimSharedLeadingWhitespace = (content) => {
 };
 
 const processCmsComponentTemplate = (content, name, template, data, imports, dependencies) => {
+    const scaffoldRegexs = [
+        { source: "<!--\\s*cp-scaffold\\s*(.*?)\\s*else\\s*-->\\s*(.*?)\\s*<!--\\s*\\/cp-scaffold\\s*-->", replacement: "$1"},
+        { source: "<!--\\s*cp-scaffold\\s*(.*?)\\s*\\/cp-scaffold\\s*-->", replacement: "$1"}
+    ];
     const fieldRegexs = [
         { source: "{{.*?\\/\\*.*?%%name%%.*?\\*\\/.*?}}", replacement: "<!-- {%%fieldname%%:%%fieldtype%%} -->" },
         { source: "{{.*?%%name%%.*?}}", replacement: "{%%fieldname%%:%%fieldtype%%}" },
@@ -143,6 +147,16 @@ const processCmsComponentTemplate = (content, name, template, data, imports, dep
         { source: "<([a-z0-9:-]*)(\\s*.*?)\\s+component\\s*=\\s*([\"'])([^\"']+)\\3([^>]*?)(><\\/\\1>|\\/>)", replacement: "<$1$2$5>{%%name%%}</$1>" }
     ];
     let result = template;
+    for (let j = 0, lenJ = scaffoldRegexs.length; j < lenJ; j++) {
+        let regex = new RegExp(scaffoldRegexs[j].source);
+        let match = regex.exec(result);
+        while (match) {
+            let replacement = scaffoldRegexs[j].replacement;
+            //console.log(`Replacing [${match[0]}] with [${replacement}]`);
+            result = result.replace(regex, replacement);
+            match = regex.exec(result);
+        }
+    }
     // Longest name first to avoid substring replacements
     var dataItems = data.sort((a, b) => b.name.length - a.name.length);
     for (let i = 0, len = dataItems.length; i < len; i++) {
